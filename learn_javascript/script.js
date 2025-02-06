e81f28c480edc72c9caeffc51c3d9bd41f6e7def
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2025-02-06 11:50:29 +0530

    Concept of let, var, const

---
+++ learn_javascript/script.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+let a = 5;
+console.log(a);
+
+let ram = "Lord"
+ram = "Rama"
+console.log(ram);
+
+const b = 10;
+console.log(b);
+// b = 20;
+// console.log(b);      // Error: Assignment to constant variable.
+
