ad684dba21e7819d823791824214cec4e92a4e4b
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2025-02-17 10:32:39 +0530

    Conditions done

---
+++ learn_javascript/conditions.js
@@ -0,0 +1,50 @@
+let age = 20;
+
+if(age > 18) {
+    console.log("Can drive a car");
+}
+else {
+    console.log("Cannot drive a car");
+}
+
+// else if statement
+
+let num = 20;
+
+if (num > 10) {
+    console.log("2nd number");
+}
+
+else if (num < 0) {
+    console.log("1st number");
+}
+
+else if (num = 20) {
+    if (age > 18) {
+        console.log("Can drive a car");
+    }
+
+    console.log("3rd number");
+}
+else {
+    console.log("Zero");
+}
+
+
+
+// switch statement
+
+let number = 2;
+
+switch(number) {
+    case 1 : console.log("One");
+    break;
+    case 2 : console.log("Two");
+    break;
+    case 3 : console.log("Three");
+    break;
+    case 4 : console.log("Four");
+    break;
+    default : console.log("Invalid number");
+}
+
