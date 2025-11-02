42c1cc890a50c4c0f03deca9305248f1f7848bee
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-02 12:22:04 +0530

    Swap two values using third variable

--- learn_javascript/let_var_const.js
+++ learn_javascript/let_var_const.js
@@ -1,17 +1,43 @@
 console.log("Hello, World!");
 
+// let a = 5;
+// console.log(a);
+
+// let ram = "Lord"
+// ram = "Rama"
+// console.log(ram);
+
+// const b = 10;
+// console.log(b);
+
+// b = 20;
+// console.log(b);      // Error: Assignment to constant variable.
+
+
+
+
+// Swapping values using third variable
+
+// let a = 5;
+// let b= 10;
+// let c;
+
+// c=a;
+// a=b;
+// b=c; 
+// console.log("a:", a);
+// console.log("b:", b);
+
+
+// Swapping values without using third variable
+
 let a = 5;
-console.log(a);
+let b = 10;
 
-let ram = "Lord"
-ram = "Rama"
-console.log(ram);
+a= a +b;
+b= a -b;
+a= a-b;
 
-const b = 10;
-console.log(b);
-
-b = 20;
-console.log(b);      // Error: Assignment to constant variable.
-
-
+console.log("a:", a);
+console.log("b:", b);
 
