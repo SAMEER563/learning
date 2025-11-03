7574141364e8445717485cd37b647c109c0e34cd
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-03 11:44:29 +0530

    Math function

---
+++ learn_javascript/math_function.js
@@ -0,0 +1,39 @@
+
+
+// Math.floor() - Rounds a number down to the nearest integer
+
+console.log(Math.floor(5.5))
+
+// Math.ceil() - Rounds a number up to the nearest integer
+
+console.log(Math.ceil(5.2))
+
+// Math.round() - Rounds a number to the nearest integer
+
+console.log(Math.round(5.5))
+console.log(Math.round(5.4))
+
+// Math.sqrt() - Returns the square root of a number
+
+console.log(Math.sqrt(25))
+
+// Math.abs() - Returns the absolute value of a number means positive value from negative value
+
+console.log(Math.abs(-10))
+
+// Math.pow() - Returns the value of a number raised to the power of another number
+
+console.log(Math.pow(2, 3)) //2^3=8
+
+// Math.min() - Returns the smallest of zero or more numbers
+
+console.log(Math.min(5, 10, 2, 8))
+
+// Math.max() - Returns the largest of zero or more numbers
+console.log(Math.max(5, 10, 2, 8))
+
+
+// Generate otp between 1000 to 9999
+
+console.log(Math.floor(1000 + Math.random() * 9000));
+
