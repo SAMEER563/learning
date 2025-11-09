abb0575e5df5215ec8e397e001a288767fcbf4aa
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-09 20:30:24 +0530

    Question 3

--- learn_javascript/let_var_const.js
+++ learn_javascript/let_var_const.js
@@ -74,3 +74,10 @@ for (var i = 0; i < 3; i++) {
 } 
 // Output: 3 3 3
 
+// What will be the output of the following code?
+
+for (let j = 0; j < 3; j++) {
+  setTimeout(() => console.log(j), 1000);
+}
+
+// Output: 0 1 2
