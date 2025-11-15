407e6429841ea906b142d8d729914f26a86057a5
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-15 12:27:18 +0530

    Arrow function for Question 1

--- learn_javascript/function.js
+++ learn_javascript/function.js
@@ -58,3 +58,17 @@ function countVowels(str) {
     return count;
 }
 console.log(countVowels("Hello World")); // Output: 3
+
+// Create an arrow function for same question.
+
+const countVowelsArrow = str => {
+    let count = 0;
+    for (let char of str) {
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowelsArrow("Hello World")); // Output: 3
