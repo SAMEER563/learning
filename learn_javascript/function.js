397cc96c5b17b0d16726c6037f491c7ac2a18d24
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-15 12:14:40 +0530

    function expression

--- learn_javascript/function.js
+++ learn_javascript/function.js
@@ -6,9 +6,29 @@ function sum () {
 sum(); // Output: Hi there!
 
 
+
+
 // function with parameters
 
 function add (a, b) {
     console.log(a + b);
 }
 add(5, 10); // Output: 15
+
+
+
+// function with return value
+function multiply (a, b) {
+    return a * b;
+}  
+console.log(multiply(5, 10)); // Output: 50
+
+
+// function expression
+
+const divide = function (a, b) {
+    return a / b;
+};
+console.log(divide(10, 2)); // Output: 5
+
+
