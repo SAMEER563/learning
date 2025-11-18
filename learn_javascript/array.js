a1911409fde994d8f9e98f63872260bba0de2fa8
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-18 11:49:09 +0530

    Find the max number for array

--- learn_javascript/array.js
+++ learn_javascript/array.js
@@ -8,10 +8,37 @@
 // console.log("Length of array:", arr.length); // Output: Length of array: 5
 
 
-let arr = new Array(5);
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = Number(prompt("Enter number for index "));
-    sum = sum + arr[i];
+// let arr = new Array(5);
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = Number(prompt("Enter number for index "));
+//     sum = sum + arr[i];
+// }
+// console.log("Sum of array elements:", sum); // Output: Sum of array elements: 0
+
+
+// Find max number from array
+
+let arr = [10, 5, 8, 20, 3];
+let max = arr[0];
+for (let i= 0; i<arr.length; i++) {
+    if (max < arr[i]) {
+        max = arr[i];
+    }
 }
-console.log("Sum of array elements:", sum); // Output: Sum of array elements: 0
+console.log("Maximum number in the array:", max); // Output: Maximum number in the array: 20
+
+
+// Find min number from given array
+
+// let arr = [10, 5, 8, 20, 3];
+// let min = arr[0];
+// for (let i = 0; i <arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }
+// console.log("Minimum number in the array:", min); // Output: Minimum number in the array: 3
+
+
+
