ec9d69d4e00ee8e4c4f6320d0fd7fb38f79af79e
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-25 11:40:32 +0530

    Rotate an array by k position to the left

--- dsa_journey/array_ques.js
+++ dsa_journey/array_ques.js
@@ -1,14 +1,26 @@
 
 
 // Rotate an array by one position to the left
-let arr = [1,2,3,4,5]
-let copy = arr[0]
-for(let i=0; i<arr.length-1; i++) {
+// let arr = [1,2,3,4,5]
+// let copy = arr[0]
+// for(let i=0; i<arr.length-1; i++) {
+//     arr[i] = arr[i+1]
+// }
+//  arr[arr.length-1] = copy
+//  console.log(arr)  // Output: [2, 3, 4, 5, 1]
+
+
+ // Rotate an array by k position to the left
+    let arr = [1,2,3,4,5]
+    let k = 2
+    for(let j=0; j<k; j++) {
+       let copy = arr[0]
+    for(let i=0; i<arr.length-1; i++) {
     arr[i] = arr[i+1]
 }
- arr[arr.length-1] = copy
- console.log(arr)  // Output: [2, 3, 4, 5, 1]
-
+    arr[arr.length-1] = copy
+    }
+    console.log(arr)  // Output: [3, 4, 5, 1, 2]
 
 
 
