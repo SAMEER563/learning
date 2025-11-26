e98a447b52aa3dd8392a731af4067b7661059a1f
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-26 21:43:53 +0530

    new question

--- dsa_journey/array_ques.js
+++ dsa_journey/array_ques.js
@@ -37,16 +37,16 @@
 //  console.log(arr) // Output: [5, 1, 2, 3, 4]
 
 // Rotate an array by k position to the right
-    let arr = [1,2,3,4,5]
-    let k = 2
-    for(let j=0; j<k; j++) {
-       let copy = arr[arr.length-1]
-    for(let i=arr.length-1; i>0; i--) {
-        arr[i] = arr[i-1]
-    }
-    arr[0] = copy
-    }
-    console.log(arr)  // Output: [4, 5, 1, 2, 3]
+    // let arr = [1,2,3,4,5]
+    // let k = 2
+    // for(let j=0; j<k; j++) {
+    //    let copy = arr[arr.length-1]
+    // for(let i=arr.length-1; i>0; i--) {
+    //     arr[i] = arr[i-1]
+    // }
+    // arr[0] = copy
+    // }
+    // console.log(arr)  // Output: [4, 5, 1, 2, 3]
 
 
 
