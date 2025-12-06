fc78b8a371fe592dae5099c997782077788c2c4f
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-12-06 21:00:23 +0530

    Two questions solve of string

---
+++ learn_javascript/string.js
@@ -0,0 +1,11 @@
+// What is the output of the following code?
+
+console.log("Hello".toUpperCase());
+// Output: "HELLO"
+
+//How do you get the first character of a string?
+
+let str = "JavaScript";
+console.log(str.charAt(0));
+// Output: "J"
+
