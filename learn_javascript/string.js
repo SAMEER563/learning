fdb77c596695a0dd694c2a3e7d6f127b6fce9445
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-12-06 21:04:13 +0530

    Solve two another questions

--- learn_javascript/string.js
+++ learn_javascript/string.js
@@ -9,3 +9,14 @@ let str = "JavaScript";
 console.log(str.charAt(0));
 // Output: "J"
 
+// What will be the result of the following code?
+
+let str1 = "JavaScript";
+console.log(str1.slice(0, 4));
+// Output: "Java"
+
+
+// How can you check if a string contains a specific substring (e.g., "world" in "Hello world")?
+let str2 = "Hello world";
+console.log(str2.includes("world"));
+// Output: true
