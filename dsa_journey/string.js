1b0c693bfbb6adbfb2dd17b6a67a8a0ef460093f
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-12-13 10:49:08 +0530

    Reverse a string by 1st method

--- dsa_journey/string.js
+++ dsa_journey/string.js
@@ -6,3 +6,12 @@ for(let i= 0; i<str.length; i++) {
     console.log(str[i]);
 }
 
+
+// Reverse a string
+
+let str1 = "SAMEER";
+let rev = "";
+for(let i = str1.length - 1; i >= 0; i--) {
+    rev = rev + str1.charAt(i);
+}
+console.log(rev);
