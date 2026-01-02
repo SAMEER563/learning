9d23b5a04a1b2723aef0ae14797b3cf3e37fe19b
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-01-02 20:21:30 +0530

    Another approach using a map to store the last seen index of characters

--- dsa_journey/sliding_window.js
+++ dsa_journey/sliding_window.js
@@ -15,3 +15,19 @@ function lengthOfLongestSubstring(s) {
     }
     return maxLength;
 }
+
+// Another approach using a map to store the last seen index of characters
+
+function lengthOfLongestSubstringMap(s) {
+    let map = new Map();
+    let left = 0;
+    let maxLength = 0;
+    for(let right=0; right<s.length; right++) {
+        if(map.has(s[right])) {
+            left = Math.max(map.get(s[right]) + 1, left);
+        }
+        map.set(s[right], right);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    return maxLength;
+}
