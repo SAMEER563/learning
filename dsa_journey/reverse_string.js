9e3d7867e61384a8fbed5c9946797f50c2b7848a
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-01-07 22:43:54 +0530

    reverse a string

--- dsa_journey/reverse_string.js
+++ dsa_journey/reverse_string.js
@@ -1,45 +1,14 @@
 
-// Reverse a String
-function revString (str) {
-    return str.split("").reverse().join("")
-
-}
- 
-console.log(revString("SAMEER"))
-
-// Sum of Array Elements
-let arr = [1,2,3,4,4,5,6,6]
-let sum = 0;
-for (let i=0; i<arr.length; i++) {
-    sum = sum + arr[i]
-}
-  
-console.log(sum)
-
-// Check for Palindrome
-function isPalindrome(str) {
-    let left = 0, right = str.length-1;
-    while(left < right ) {
-      if (str[left] !== str[right])
-        return false;
-        left ++;
-        right --;
-      
+function reverseString(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        // Swap characters
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
     }
-    return true
+    return s;
 }
-
-console.log(isPalindrome("mdjs"))
-
-// Remove Duplicates from Array
-function removeDuplicates(arr){
-    return [...new Set(arr)];
-}
-
-console.log(removeDuplicates([1,75,4,5,7,5,4,7]))
-
-
-function countDigit(num){
- return num.toString().length;
-}
-console.log(countDigit(578469))
