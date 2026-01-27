bc9751134a84110018e6d7a586c9afaaa8561fa4
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-01-27 14:07:04 +0530

    edit the value of object

--- learn_javascript/object.js
+++ learn_javascript/object.js
@@ -3,7 +3,13 @@ let user = {
     age: 30,
     email: "ramu@example.com",
 }
+
+// Accessing object properties
 console.log(user.name);  // Output: Ramu
 console.log(user["age"]);  // Output: 30
 console.log(user.age);  // Output: 30
 console.log(typeof user);  // Output: object
+
+// Modifying object properties
+user.age = 31;
+console.log(user.age);  // Output: 31
