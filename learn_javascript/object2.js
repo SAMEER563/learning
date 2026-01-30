db757b4abaec7cd906c7b9da126acbb627413306
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-01-30 22:38:56 +0530

    use of this keyword

--- learn_javascript/object2.js
+++ learn_javascript/object2.js
@@ -6,10 +6,24 @@ let user = {
     age: 30,
     email: "ramu@example.com",
     greet: function() {
-        console.log("Hello");
+        console.log("Hello " + user.name);
         return 20;
     }
 }
 
 let vl = user.greet();  
 console.log(vl);  // Output: 20
+
+// why we use 'this' keyword
+let user2 = {
+    name: "Shyam",
+    age: 30,
+    email: "ramu@example.com",
+    greet: function() {
+        console.log("Hello " + this.name);  // 'this' refers to the current object
+    }
+}
+
+user2.greet();  // Output: Hello Shyam
+// 'this' is useful when the object name is not known or may change
+
