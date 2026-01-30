36ad7eb7e776f6346496683503c8f6c23859cccb
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-01-30 22:39:19 +0530

    when the object name is not known or may change then use if this keyword

--- learn_javascript/object2.js
+++ learn_javascript/object2.js
@@ -27,3 +27,16 @@ let user2 = {
 user2.greet();  // Output: Hello Shyam
 // 'this' is useful when the object name is not known or may change
 
+//  example on when the object name is not known or may change
+function createUser(name, age) {
+    return {
+        name: name,
+        age: age,
+        greet: function() {
+            console.log("Hello " + this.name);
+        }
+    }
+}
+
+let user3 = createUser("Mohan", 25);
+user3.greet();  // Output: Hello Mohan
