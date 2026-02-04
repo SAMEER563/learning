79ca3cad799d2e1f46794b2d311a9cfbf488f7dd
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-04 12:49:00 +0530

    Access other string in a string

--- learn_javascript/string1.js
+++ learn_javascript/string1.js
@@ -4,3 +4,8 @@ let str = "Hello Ram"
 let str1 = 'Hi Shyam'
 let str2 = `Looking good`
 
+// Access other string in a string 
+
+let str4 = 10
+let str5 = `You will get ${str4} in each subject`
+console.log(str5)
