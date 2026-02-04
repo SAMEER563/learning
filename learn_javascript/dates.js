8e5eb69d79b0844e2880c772a2c9029a0729d127
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-04 13:55:22 +0530

    Get time and date in various format

--- learn_javascript/dates.js
+++ learn_javascript/dates.js
@@ -2,3 +2,9 @@
 let date = new Date();
 console.log(date)  // It returns ITC date and time 
 console.log(date.toString()) // It returns GMT time 
+console.log(date.toDateString())  // It returns only date
+console.log(date.toISOString())   // It returns UTC time
+console.log(date.toLocaleString()) // It returns string date and time 
+console.log(date.toLocaleDateString())  // It returns string date 
+
+
