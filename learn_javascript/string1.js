15f3a46325302ac5de304ba23fdd5df0e4cbf174
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-04 12:57:19 +0530

    Find the substring

--- learn_javascript/string1.js
+++ learn_javascript/string1.js
@@ -1,19 +1,24 @@
 // Declaration of string 
 
-let str = "Hello Ram"
-let str1 = 'Hi Shyam'
-let str2 = `Looking good`
+// let str = "Hello Ram"
+// let str1 = 'Hi Shyam'
+// let str2 = `Looking good`
 
 // Access other string in a string 
 
-let str4 = 10
-let str5 = `You will get ${str4} in each subject`
-console.log(str5)
+// let str4 = 10
+// let str5 = `You will get ${str4} in each subject`
+// console.log(str5)
 
 // Change string into capital letter
 
-console.log(str.toUpperCase())
+// console.log(str.toUpperCase())
 
 // Change string into lower case 
 
-console.log(str1.toLowerCase())
+// console.log(str1.toLowerCase())
+
+// Find the substring 
+
+let str = `Rahul is bad Boy`
+console.log(str.indexOf('bad'))
