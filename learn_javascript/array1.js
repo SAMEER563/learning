e47e45a99c38cbcdb202c5594a2b3d146abe78d3
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-05 12:47:53 +0530

    Use iteration to print one by one

--- learn_javascript/array1.js
+++ learn_javascript/array1.js
@@ -1,20 +1,28 @@
-// Declare array 
+// // Declare array 
 
-let arr = [12, 50, "Ram", true]
-console.log(arr)
+// let arr = [12, 50, "Ram", true]
+// console.log(arr)
 
-// Push operation - Add new element at the end of array
-arr.push("Shalu");
-console.log(arr);
+// // Push operation - Add new element at the end of array
+// arr.push("Shalu");
+// console.log(arr);
 
-// Pop operation - Remove last element from the array
-arr.pop();
-console.log(arr)
+// // Pop operation - Remove last element from the array
+// arr.pop();
+// console.log(arr)
 
-// Unshift method - Use to add new element at starting 
-arr.unshift("sam")
-console.log(arr)
+// // Unshift method - Use to add new element at starting 
+// arr.unshift("sam")
+// console.log(arr)
 
-// Shift method - Use to remove first element 
-arr.shift();
-console.log(arr)
+// // Shift method - Use to remove first element 
+// arr.shift();
+// console.log(arr)
+
+// Use iteration to print one by one 
+
+let arr1 = [10,52,45,75,56]
+
+for(i=0; i<arr1.length; i++){
+    console.log(arr1[i])
+}
