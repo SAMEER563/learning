7d06a13b762958589c00a89ad84baf9bc0101e2a
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-05 11:36:30 +0530

    Pop operation

--- learn_javascript/array1.js
+++ learn_javascript/array1.js
@@ -4,5 +4,9 @@ let arr = [12, 50, "Ram", true]
 console.log(arr)
 
 // Push operation - Add new element at the end of array
-arr.push("Shalu")
+arr.push("Shalu");
+console.log(arr);
+
+//Pop operation - Remove last element from the array
+arr.pop();
 console.log(arr)
