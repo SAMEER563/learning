3dd1efbc58f0c1ae90434969d05f741a1643d0c9
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-05 13:15:55 +0530

    Join two array using spread operator

--- learn_javascript/array1.js
+++ learn_javascript/array1.js
@@ -54,6 +54,14 @@
 
 // Uses of splice - It give the value which I required by using index but it make changes into original array
 
+// let arr = [10,52,45,75,56];
+// console.log(arr.splice(1,3))
+// console.log(arr)
+
+// Join two array using spread operator
+
 let arr = [10,52,45,75,56];
-console.log(arr.splice(1,3))
-console.log(arr)
+let arr1 = ["Ram", 85, true];
+
+let arr3 = [...arr , ...arr1]
+console.log(arr3)
