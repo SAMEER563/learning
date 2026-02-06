93b1b943291b3a0470e04d94ce9ad17727436070
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-06 16:55:13 +0530

    write function part

---
+++ learn_javascript/function1.js
@@ -0,0 +1,45 @@
+// Make a greet function 
+
+// function greet() {
+//     console.log("Welcome Sam")
+// }
+
+// greet(); // Function calling
+
+// Add two number with function 
+
+// function addNum (num1, num2){
+//    sum = num1 + num2
+//    console.log( sum)
+// }
+
+// addNum(2,5)
+
+// Add multiple numbers by using rest opretor (...)
+
+// function addNum(...nums) {
+//     let sum = 0;
+//     for (let s of nums) {
+//         sum += s
+//     }
+//     console.log(sum)
+// }
+// addNum(2, 5);
+// addNum(5,6,8)
+
+// Second way to create function by expression
+
+// var addNum = function (){
+//     console.log("object")
+// }
+
+// addNum();
+
+
+// Third way to create function - Arrow function ()=>{}
+
+    var addNum = ()=> {
+        console.log("Hii")
+    }
+
+    addNum()
