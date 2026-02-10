098ffa4212f8adb28f3c5bee7c7fa66e40a991b7
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2026-02-10 22:40:42 +0530

    rev string questions

--- dsa_journey/rev_str.js
+++ dsa_journey/rev_str.js
@@ -1,8 +1,13 @@
 // Reverse string in JavaScript
 
 let str = "Hello, World!";
+// let reversedStr = str.split("").reverse().join("")
+// console.log(reversedStr); // Output: !dlroW ,olleH
+
+
+// Another way to solve
 
 let reversedStr = str.split("").map(word=>word.split("").reverse().join("")).join("");
-console.log(reversedStr); // Output: !dlroW ,olleH
 
 
+console.log(reversedStr)
